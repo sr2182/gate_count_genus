39702cd50fa4b66d7252919c7ee3701ee3b4b4e6
Saad Rahman <154258809+sr2182@users.noreply.github.com>
2024-09-19 20:48:53 -0400

    Update mult_posit16b.tcl

--- tcl/mult_posit16b.tcl
+++ tcl/mult_posit16b.tcl
@@ -1,7 +1,7 @@
 set_db lib_search_path /umbc/software/cadence/installs/GENUS21_2023_09_08/share/synth/tutorials/tech
 set_db library tutorial.lib
 
-read_hdl -sv ../srcs/sources/pacogen_multiplier_16.sv ../srcs/sources/mult_N32_ES6_PIPE6_v1.v
+read_hdl -sv ../srcs/pacogen_multiplier_16.sv ../srcs/mult_N32_ES6_PIPE6_v1.v
 elaborate 
 check_design -unresolved posit_multiplier_16
 
