ed20b41e8c50657f5b9b16b93f3bbea6cce3d46c
Saad Rahman <154258809+sr2182@users.noreply.github.com>
2024-09-19 20:47:34 -0400

    Update mult_24b.tcl

--- tcl/mult_24b.tcl
+++ tcl/mult_24b.tcl
@@ -1,7 +1,7 @@
 set_db lib_search_path /umbc/software/cadence/installs/GENUS21_2023_09_08/share/synth/tutorials/tech
 set_db library tutorial.lib
 
-read_hdl -sv ../srcs/sources/fp_mult_24b.sv ../srcs/sources/FloatingMultiplication_v1.v
+read_hdl -sv ../srcs/fp_mult_24b.sv ../srcs/FloatingMultiplication_v1.v
 elaborate 
 check_design -unresolved fp_mult_24b
 
